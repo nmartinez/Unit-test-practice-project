1597b889c1f342e8319d3dfb06abbc1d29fe7679
Naileth Martinez <nmartinez@bgtpartners.com>
2012-01-17 14:49:43 -0400

    segundo cambio

--- js/old-fuctions.js
+++ js/old-fuctions.js
@@ -2,10 +2,14 @@ $(document).ready(function() {
         
 		var cant = 0;
 		$('input[type=checkbox]').click(function(){
-                cant++;
-				if(cant > 4){
+            if($('.checkbox').cheched){
+			  cant++;
+			  if(cant > 4){
 					alert('Maximum of 4 comparison parts');
-					cant=0;
 				}
-        }); 
+			}
+			else{
+				cant--;
+			}
+        });	
 });
