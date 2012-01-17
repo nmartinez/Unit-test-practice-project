b93de24fda54add61eeb3eb03b7082d14267b2e3
Naileth Martinez <nmartinez@bgtpartners.com>
2012-01-17 16:02:08 -0400

    tercer cambio

--- js/old-fuctions.js
+++ js/old-fuctions.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
         
 		var cant = 0;
 		$('input[type=checkbox]').click(function(){
-            if($('.checkbox').cheched){
+            if($(this).attr("checked")){
 			  cant++;
 			  if(cant > 4){
 					alert('Maximum of 4 comparison parts');
